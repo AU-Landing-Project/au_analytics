92cbfbf0a9c492fa1f8b176615765a438369b796
Matt Beckett <beck24@gmail.com>
2012-06-07 01:00:28 -0600

    removed copy of function that moved to functions.php

--- start.php
+++ start.php
@@ -69,20 +69,4 @@ function au_analytics_page_handler($page){
   return TRUE;
 }
 
-// returns bool based on whether the current browser is IE < 9
-function au_analytics_check_ie_pre9(){
-  $match=preg_match('/MSIE ([0-9]\.[0-9])/',$_SERVER['HTTP_USER_AGENT'],$reg);
-  if($match==0){
-    return FALSE;
-  }
-
-  $version = floatval($reg[1]);
-  
-  if($reg[1] < 9){
-    return TRUE;
-  }
-  
-  return FALSE;
-}
-
 elgg_register_event_handler('init', 'system', 'au_analytics_init');
